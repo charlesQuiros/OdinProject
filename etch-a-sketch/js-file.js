e053f598e23afb52f38ce495391d95fd1258c93d
Charles Quiros <charles.quiro@gmail.com>
2021-11-25 08:32:02 -0800

    added the event listener and css class to allow drawing

--- etch-a-sketch/js-file.js
+++ etch-a-sketch/js-file.js
@@ -1,5 +1,3 @@
-//TODO create a fixed grid of 64X64
-
 const gridBg = document.getElementById("grid-bg");
 
 const makeGrid = (grid) => {
@@ -10,4 +8,14 @@ const makeGrid = (grid) => {
   }
 };
 
-makeGrid(24);
+makeGrid(64);
+
+const gridItems = document.querySelectorAll(".grid-item");
+
+gridItems.forEach((item) => {
+  item.addEventListener("mouseenter", (e) => colorGrid(e));
+});
+
+const colorGrid = (e) => {
+  e.target.className = "draw";
+};
--- etch-a-sketch/readme.txt
+++ etch-a-sketch/readme.txt
@@ -1,5 +1,5 @@
-TODO Create a fixed container for the grid 
+
+TODO Create a "hover" feature that makes the mouse highlight each grid-item 
+
 TODO Create an input for the user to adjust the grid resolution
-TODO Create buttons to draw in black and white, erase, and rainbow
-TODO 
 TODO on Wipe make the grid-bg shake and the grid wipe slowly into a blank canvas, then return to a normal clean grid
